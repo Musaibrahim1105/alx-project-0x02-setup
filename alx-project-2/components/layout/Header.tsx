61022680e73c01b032b6ad2254d8b4ece4a1faa1
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-08-27 07:40:18 +0100

    alx project 0x02 remodified

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { HeaderProps } from '@/interfaces'
+import Link from 'next/link'
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
     </div>
   )
 }
