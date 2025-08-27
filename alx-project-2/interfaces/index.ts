ce4e0a5ca5dd11f4783946b3784112921127987c
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-08-27 10:33:43 +0100

    done

--- alx-project-2/interfaces/index.ts
+++ alx-project-2/interfaces/index.ts
@@ -1,3 +1,8 @@
 export interface HeaderProps {
   title: string;
 }
+
+export interface CardProps {
+  title: string;
+  content: string;
+}
--- alx-project-2/pages/home.tsx
+++ alx-project-2/pages/home.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Card from '@/components/common/Card'
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <Card title="Card Title" content="This is the card content." />
+      <Card title="Another Card Title" content="This is some more card content." />
+      <Card title="Third Card Title" content="This is the third card content." />
     </div>
   )
 }
